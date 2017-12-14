780a6dbb2e033c5913cdc551a7c70f43c85bc6a5
Nikhil Warke <nwarke2@sapient.com>
2017-12-14 16:21:40 +0530

    Inject http service

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IProduct } from './product';
 import { OnInit } from '@angular/core/src/metadata/lifecycle_hooks';
+import { ProductService } from './product.service';
 
 @Component({
   selector: 'pm-products',
@@ -8,12 +9,17 @@ import { OnInit } from '@angular/core/src/metadata/lifecycle_hooks';
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
+  errorMessage: any;
   pageTitle: string = 'Product List';
   imageWidth: number = 50;
   imageMargin: number = 2;
   showImage: boolean = false;
-
+  filteredProducts: IProduct[];
+  products: IProduct[];
   _listFilter: string;
+
+  constructor(private _productService: ProductService) {}
+
   public get listFilter(): string {
     return this._listFilter;
   }
@@ -23,33 +29,6 @@ export class ProductListComponent implements OnInit {
       ? this.performFilter(this.listFilter)
       : this.products;
   }
-
-  filteredProducts: IProduct[];
-
-  products: IProduct[] = [
-    {
-      productId: 1,
-      productName: 'Leaf Rake',
-      productCode: 'GDN-0011',
-      releaseDate: 'March 19, 2016',
-      description: 'Leaf rake with 48-inch wooden handle.',
-      price: 19.95,
-      starRating: 3.2,
-      imageUrl:
-        'http://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png'
-    },
-    {
-      productId: 2,
-      productName: 'Garden Cart',
-      productCode: 'GDN-0023',
-      releaseDate: 'March 18, 2016',
-      description: '15 gallon capacity rolling garden cart',
-      price: 32.99,
-      starRating: 4.2,
-      imageUrl:
-        'http://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png'
-    }
-  ];
 
   performFilter(filterBy: string): IProduct[] {
     return this.products.filter(
@@ -68,6 +47,10 @@ export class ProductListComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    console.log('OnInit called');
+    this._productService.getProducts().subscribe(products => {
+      this.products = products;
+      this.filteredProducts = this.products;
+    }, error => (this.errorMessage = <any>error));
+    this.filteredProducts = this.products;
   }
 }
---
+++ APM-Start/src/app/products/product.service.ts
@@ -0,0 +1,24 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { IProduct } from './product';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/do';
+
+@Injectable()
+export class ProductService {
+  productUrl: string = './api/products/products.json';
+  constructor(private _http: HttpClient) {}
+
+  getProducts(): Observable<IProduct[]> {
+    return this._http
+      .get<IProduct[]>(this.productUrl)
+      .do(data => console.log('All: ' + JSON.stringify(data)))
+      .catch(this.handleError);
+  }
+
+  private handleError(err: HttpErrorResponse) {
+    console.log(err.message);
+    return Observable.throw(err.message);
+  }
+}
